9038df931ccd79266aeecdeab08f6a9ab38e7be4
Andrew Merenbach <andrew@merenbach.com>
2012-06-10 16:40:14 -0700

    add CSRF support for ciphers

--- ciphers/views.py
+++ ciphers/views.py
@@ -1,8 +1,8 @@
 # Create your views here.
-from django.views.generic import TemplateView
-
-class CiphersView(TemplateView):
-    template_name = 'ciphers/ciphers_index.html'
-
-    def get_context_data(self, **kwargs):
-        return {'shortcut': 'ciphers'}
+#from django.views.generic import TemplateView
+#
+#class CiphersView(TemplateView):
+#    template_name = 'ciphers/ciphers_index.html'
+#
+#    def get_context_data(self, **kwargs):
+#        return {'shortcut': 'ciphers'}
--- core/settings_base.py
+++ core/settings_base.py
@@ -119,7 +119,7 @@ STATICFILES_FINDERS = (
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+    'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -163,6 +163,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
         'django.core.context_processors.static',
         #'cms.context_processors.media',
         'sekizai.context_processors.sekizai',
+	'django.core.context_processors.csrf',
         #'zinnia.context_processors.version',    # optional
         #'django_mobile.context_processors.flavour',
         #'admintools_bootstrap.context_processors.site',
