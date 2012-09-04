60172bf3aa000a97d345b9ccf80e7c4b9c85aedc
Andrew Merenbach <andrew@merenbach.com>
2012-09-04 09:19:34 -0700

    Add settings-example, clean up settings-base

---
+++ den/settings-example.py
@@ -0,0 +1,76 @@
+# Django settings for project project.
+
+# import central settings
+from settings_base import *
+
+# for url/path manipulation
+from urlparse import urljoin
+from os.path import join
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    ('Andrew Merenbach', 'andrew@merenbach.com'),
+)
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'merenbach_django',                      # Or path to database file if using sqlite3.
+        'USER': 'merenbach_django',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '/var/run/postgresql',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+MEDIA_ROOT = ''
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+if not DEBUG:
+    MEDIA_URL = 'http://cdn.merenbach.com/'
+else:
+    MEDIA_URL = 'http://media.merenbach.com'
+
+# Absolute path to the directory static files should be collected to.
+STATIC_ROOT = join(MEDIA_ROOT, 'static')
+
+# URL prefix for static files.
+STATIC_URL = urljoin(MEDIA_URL, 'static/')
+
+# deprecated but apparently still necessary for some things
+ADMIN_MEDIA_PREFIX = urljoin(STATIC_URL, 'admin/')
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = ''
+
+# Disqus
+DISQUS_USER_API_KEY = ''
+DISQUS_FORUM_SHORTNAME = 'merenbach'
+
+# Clicky
+CLICKY_SITE_ID = ''
+#CLICKY_RENDER_NON_JS_CODE = False
+
+# my customizations
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'noreply@merenbach.com'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'noreply@merenbach.com'
+SERVER_EMAIL = 'noreply@merenbach.com'
+
+# caching
+#CACHES = {
+#        'default': {
+#            'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+#            'LOCATION': '127.0.0.1:11211',
+#            #'LOCATION': 'unix:/var/run/memcached/memcached.pid',
+#            }
+#        }
+
--- den/settings_base.py
+++ den/settings_base.py
@@ -1,49 +1,32 @@
 # Django settings for project project.
 
-import os
-from sys import path as sys_path
-from urlparse import urljoin
+import os, sys
 
-# add to default paths
-paths = (
-        '/srv/www/merenbach.com/django',
-        '/srv/www/merenbach.com/django/den',
-        #'/srv/www/merenbach.com/django/lib/python2.7',
-        )
+SITE_ID = 1
 
-for path in paths:
-    if path not in sys_path:
-        sys_path.insert(0, path)
+# Python dotted path to the WSGI application used by Django's runserver.
+WSGI_APPLICATION = 'den.wsgi.application'
 
 # get the settinsg path
-from django.utils import importlib
-_SETTINGS_MODULE = importlib.import_module(os.environ.get('DJANGO_SETTINGS_MODULE'))
-
-PROJECT_ROOT = os.path.realpath(os.path.dirname(_SETTINGS_MODULE.__file__))
+PROJECT_ROOT = os.path.realpath(os.path.dirname(__file__))
 PROJECT_PATH = os.path.abspath(PROJECT_ROOT)
 
-if PROJECT_PATH not in sys_path:
-    sys_path.insert(0, PROJECT_PATH)
+if PROJECT_PATH not in sys.path:
+    sys.path.insert(0, PROJECT_PATH)
 
-# for core project files
-CORE_PROJECT_ROOT = os.path.realpath(os.path.dirname(__file__))
-CORE_PROJECT_PATH = os.path.abspath(CORE_PROJECT_ROOT)
-
-if CORE_PROJECT_PATH not in sys_path:
-    sys_path.insert(0, CORE_PROJECT_PATH)
+# add to default paths
+#paths = (
+#	PROJECT_PATH,
+#        #os.path.realpath(os.path.dirname(PROJECT_PATH)),
+#        )
+#
+#for path in paths:
+#    if path not in sys_path:
+#        sys_path.insert(0, path)
 
 #OVERLOAD_SITE = os.environ.get('OVERLOAD_SITE')
 #OVERLOAD_SITE_MODULE = "websites.{}".format(OVERLOAD_SITE)
 #exec ("from {}.settings import *".format(OVERLOAD_SITE_MODULE))
-
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
-
-#for the contact form
-CONTACT_RECIPIENTS = tuple()
-
-#if SITE_CONTACT_RECIPIENTS:
-#    CONTACT_RECIPIENTS += SITE_CONTACT_RECIPIENTS
 
 PREPEND_WWW = True
 
@@ -73,33 +56,8 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
-## Absolute filesystem path to the directory that will hold user-uploaded files.
-## Example: "/home/media/media.lawrence.com/media/"
-#MEDIA_ROOT = '/home/merenbach/webapps/media/project/'
-
-## URL that handles the media served from MEDIA_ROOT. Make sure to use a
-## trailing slash.
-## Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-#MEDIA_URL = '/media/'
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-#STATIC_ROOT = '/home/merenbach/webapps/media/project/static/'
-#STATIC_ROOT = os.path.join(MEDIA_ROOT, 'static')
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
-#STATIC_URL = '/media/static/'
-#STATIC_URL = urljoin(MEDIA_URL, 'static/')
-
-#ADMIN_MEDIA_PREFIX = urljoin(STATIC_URL, 'admin/')
-
 # Additional locations of static files
 STATICFILES_DIRS = (
-    #os.path.join(PROJECT_PATH, 'websites', OVERLOAD_SITE, 'static'),
-    os.path.join(CORE_PROJECT_PATH, 'static'),
     os.path.join(PROJECT_PATH, 'static'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
@@ -140,14 +98,13 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.cache.FetchFromCacheMiddleware',
 )
 
-#ROOT_URLCONF = 'project.urls'
+ROOT_URLCONF = 'den.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'project.wsgi.application'
 
 TEMPLATE_DIRS = (
     #'/home/merenbach/webapps/django/project/templates',
-    os.path.join(CORE_PROJECT_PATH, 'templates'),
     os.path.join(PROJECT_PATH, 'templates'),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
@@ -223,65 +180,6 @@ INSTALLED_APPS = (
     'django_clicky',
 )
 
-# caching
-CACHES = {
-        'default': {
-            'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-            'LOCATION': '127.0.0.1:11211',
-            #'LOCATION': 'unix:/var/run/memcached/memcached.pid',
-            }
-        }
-
-
-#ADMIN_TOOLS_MEDIA_URL = 'http://media.merenbach.com/static/'
-
-MAINTENANCE_DISABLE_FOR_SUPERUSER = True
-
-BREADCRUMBS_AUTO_HOME = True
-#BREADCRUMBS_HOME_TITLE = 'Home'
-
-# articles settings
-#ARTICLES_AUTO_TAG = False
-#USE_ADDTHIS_BUTTON = False
-
-# analytics settings
-GOOGLE_ANALYTICS_MODEL = True
-GOOGLE_ANALYTICS_TRACK_PAGE_LOAD_TIME = True
-
-
-COMPRESS_PRECOMPILERS = (
-        #('text/coffeescript', 'coffee --compile --stdio'),
-        #('text/less', 'lessc {infile} {outfile}'),
-        #('text/x-sass', 'sass {infile} {outfile}'),
-        ('text/x-scss', 'sass --scss {infile} {outfile}'),
-        )
-
-#COMPRESS_OFFLINE = True
-COMPRESS_OFFLINE = False
-COMPRESS_STORAGE = 'compressor.storage.GzipCompressorFileStorage'
-
-HAYSTACK_CONNECTIONS = {
-        'default': {
-            'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
-            'PATH': os.path.join(PROJECT_ROOT, 'whoosh_index'),
-            #'PATH': os.path.join(os.path.dirname(__file__), 'whoosh_index'),
-            'TIMEOUT': 60 * 5,
-            'INCLUDE_SPELLING': True,
-            'BATCH_SIZE': 100,
-            },
-        #'default{}'.format(SITE_ID): {
-        #    'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
-        #    'PATH': os.path.join(os.path.dirname(__file__), 'websites', OVERLOAD_SITE, 'whoosh_index'),
-        #    #'PATH': os.path.join(os.path.dirname(__file__), 'whoosh_index'),
-        #    'TIMEOUT': 60 * 5,
-        #    'INCLUDE_SPELLING': True,
-        #    'BATCH_SIZE': 100,
-        #    },
-        }
-
-HAYSTACK_SEARCH_RESULTS_PER_PAGE = 5
-#HAYSTACK_ROUTERS = ['core.routers.SiteRouter']
-
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
@@ -311,56 +209,75 @@ LOGGING = {
     }
 }
 
-# formerly site-specific for merenbach.com
+# App-specific settings
+#ADMIN_TOOLS_MEDIA_URL = 'http://media.merenbach.com/static/'
+
+# Maintenance (currently having no effect)
+MAINTENANCE_DISABLE_FOR_SUPERUSER = True
+
+# Breadcrumbs
+#BREADCRUMBS_AUTO_HOME = True
+#BREADCRUMBS_HOME_TITLE = 'Home'
+
+# Articles
+#ARTICLES_AUTO_TAG = False
+##USE_ADDTHIS_BUTTON = False
+
+# Google Analytics
+GOOGLE_ANALYTICS_MODEL = True
+GOOGLE_ANALYTICS_TRACK_PAGE_LOAD_TIME = True
+
+# Django Compressor
+COMPRESS_PRECOMPILERS = (
+        #('text/coffeescript', 'coffee --compile --stdio'),
+        #('text/less', 'lessc {infile} {outfile}'),
+        #('text/x-sass', 'sass {infile} {outfile}'),
+        ('text/x-scss', 'sass --scss {infile} {outfile}'),
+        )
+
+#COMPRESS_OFFLINE = True
+COMPRESS_OFFLINE = False
+COMPRESS_STORAGE = 'compressor.storage.GzipCompressorFileStorage'
+
+# Haystack
+HAYSTACK_CONNECTIONS = {
+        'default': {
+            'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
+            'PATH': os.path.join(PROJECT_ROOT, 'whoosh_index'),
+            #'PATH': os.path.join(os.path.dirname(__file__), 'whoosh_index'),
+            'TIMEOUT': 60 * 5,
+            'INCLUDE_SPELLING': True,
+            'BATCH_SIZE': 100,
+            },
+        #'default{}'.format(SITE_ID): {
+        #    'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
+        #    'PATH': os.path.join(os.path.dirname(__file__), 'websites', OVERLOAD_SITE, 'whoosh_index'),
+        #    #'PATH': os.path.join(os.path.dirname(__file__), 'whoosh_index'),
+        #    'TIMEOUT': 60 * 5,
+        #    'INCLUDE_SPELLING': True,
+        #    'BATCH_SIZE': 100,
+        #    },
+        }
+
+HAYSTACK_SEARCH_RESULTS_PER_PAGE = 10
+#HAYSTACK_SEARCH_RESULTS_PER_PAGE = 5
+#HAYSTACK_ROUTERS = ['core.routers.SiteRouter']
+
 
 #MEDIA_CDN_DOMAIN = 'http://cdn.merenbach.com/'
 #DJANGO_STATIC_FILE_PROXY = 'den.cdn.cdn_origin_pull_file_proxy'
 
-
-from urlparse import urljoin
-from os.path import join
-
-SITE_ID = 1
-
-# for the contact form
+# Contact form
 CONTACT_RECIPIENTS = ('andrew@merenbach.com',)
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-MEDIA_ROOT = '/srv/www/merenbach.com/media/'
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-if not DEBUG:
-    MEDIA_URL = 'http://cdn.merenbach.com/'
-else:
-    MEDIA_URL = 'http://media.merenbach.com'
-
-# Absolute path to the directory static files should be collected to.
-STATIC_ROOT = join(MEDIA_ROOT, 'static')
-
-# URL prefix for static files.
-STATIC_URL = urljoin(MEDIA_URL, 'static/')
-
-# deprecated but apparently still necessary for some things
-ADMIN_MEDIA_PREFIX = urljoin(STATIC_URL, 'admin/')
-
-ROOT_URLCONF = 'den.urls'
-
-# DISQUS
-DISQUS_FORUM_SHORTNAME = 'merenbach'
-
-# Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'den.wsgi.application'
-
-CLICKY_SITE_ID = '66628570'
-#CLICKY_RENDER_NON_JS_CODE = False
-
+# Dajax/DajaxIce
 DAJAXICE_MEDIA_PREFIX = 'dajaxice'
 DAJAXICE_DEBUG = False
 DAJAXICE_NOTIFY_EXCEPTIONS = True
 #import logging
 #logging.basicConfig(level=logging.DEBUG)
 
+# Zinnia
 ZINNIA_MARKUP_LANGUAGE = 'markdown'
 ZINNIA_WYSIWYG = False
 from zinnia.xmlrpc import ZINNIA_XMLRPC_METHODS
