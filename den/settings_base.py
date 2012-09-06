18417578b79c84d95a9bf8d9302ab996065cff9a
Andrew Merenbach <andrew@merenbach.com>
2012-09-05 22:34:33 -0700

    Use proper WSGI path for runserver

--- den/settings_base.py
+++ den/settings_base.py
@@ -102,7 +102,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'den.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'project.wsgi.application'
+WSGI_APPLICATION = 'den.wsgi.application'
 
 TEMPLATE_DIRS = (
     #'/home/merenbach/webapps/django/project/templates',
