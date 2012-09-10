8be96de7f5442ac9900fe365455de8b8f168d1f2
Andrew Merenbach <andrew@merenbach.com>
2012-09-09 21:34:20 -0700

    Clean up software mechanics

--- software/urls.py
+++ software/urls.py
@@ -1,56 +1,9 @@
-from django.conf.urls.defaults import *
-from django.views.generic import DetailView, ListView
-from software.models import Software
-from tagging.views import tagged_object_list
+from django.conf.urls.defaults import url, patterns, include
+from software.views import SoftwareIndexView, SoftwareDetailView, SoftwareTagView
 
 urlpatterns = patterns('',
-        #url(r'^(?P<slug>[-a-z]+)/$', 'detail'),
-        #(r'^(?P<slug>[-\w]+)/$',
-        # below works
-        url(r'^$',
-            ListView.as_view(
-                #queryset=Software.objects.order_by('-pub_date')[:5],
-                #context_object_name='latest_software_list',
-                #queryset=Software.objects.filter(is_published=True).order_by('-pub_date'),
-                queryset=Software.objects.filter(is_published=True).order_by('title'),
-                context_object_name='software_list',
-                template_name='software/index.html'),
-            name='software',
-            ),
-
-        url(r'^(?P<slug>[-a-z0-9]+)/$',
-            DetailView.as_view(
-                #queryset=Software.objects.filter(is_published=True).order_by('-pub_date'),
-                queryset=Software.objects.filter(is_published=True).order_by('title'),
-                template_name='software/detail.html'),
-            name='software_detail',
-            ),
-
-        #(r'^tags/$', 'software.views.tags'),
-        #(r'^tag/(?P<tag>[^/]+)/$', 'software.views.with_tag'),
-        url(r'^tagged/(?P<tag>[^/]+)/$', tagged_object_list,
-            #dict(queryset_or_model=Software.objects.filter(is_published=True).order_by('-pub_date'), allow_empty=True,
-            dict(queryset_or_model=Software.objects.filter(is_published=True).order_by('title'), allow_empty=True,
-                template_object_name='software'),
-            name='software_tag_detail',
-            ),
-
-        #url(r'^(?P<pk>\d+)/results/$',
-        #	DetailView.as_view(
-        #		model=Poll,
-        #		template_name='polls/results.html'),
-        #	name='poll_results'),
-        #(r'^(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
-        )
-
-
-
-#urlpatterns = patterns('polls.views',
-#    # Examples:
-#    # url(r'^$', 'myproject.views.home', name='home'),
-#    # url(r'^myproject/', include('myproject.foo.urls')),
-#	(r'^$', 'index'),
-#	(r'^(?P<poll_id>\d+)/$', 'detail'),
-#	(r'^(?P<poll_id>\d+)/results/$', 'results'),
-#	(r'^(?P<poll_id>\d+)/vote/$', 'vote'),
-#)
+    url(r'^$', SoftwareIndexView.as_view(), name='software'),
+    url(r'^(?P<slug>[-a-z0-9]+)/$', SoftwareDetailView.as_view(), name='software_detail'),
+    url(r'^tagged/(?P<tag>[^/]+)/$', SoftwareTagView.as_view(), name='software_tag_detail'),
+    #(r'^tags/$', 'software.views.tags'),
+)
--- software/views.py
+++ software/views.py
@@ -1,80 +1,46 @@
-#from django.template import Context, loader
+from django.core.urlresolvers import reverse
+from django.views.generic import DetailView, ListView
+from software.models import Software
+from django.http import Http404
 
-#from django.shortcuts import render_to_response, get_object_or_404
-#from django.http import HttpResponseRedirect, HttpResponse
-#from django.core.urlresolvers import reverse
-#from django.template import RequestContext
-#from software.models import Software
-#
-#from tagging.models import Tag, TaggedItem
-#import models
-#
-#def tags(request):
-#    return shortcuts.render_to_response('software/tags.html')
-#
-#def with_tag(request, tag, object_id=None, page=1):
-#    query_tag = Tag.objects.get(name=tag)
-#    software = TaggedItem.objects.get_by_model(models.Software, query_tag)
-#    software = software.order_by('-pub_date')
-#    return render_to_response('software/with_tag.html', dict(tag=tag, software=software))
+class SoftwareIndexView(ListView):
+    template_name = 'software/index.html'
+    context_object_name = 'software_list'
+    queryset = Software.objects.filter(is_published=True).order_by('title')
 
-#from django.http import HttpResponse
+    def dispatch(self, request, *args, **kwargs):
+        request.breadcrumbs('Software', reverse('software'))
+        return super(SoftwareIndexView, self).dispatch(request, *args, **kwargs)
 
-#def index(request):
-#	latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-#	return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
-#
-##def index(request):
-##	latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-##	t = loader.get_template('polls/index.html')
-##	c = Context({
-##		'latest_poll_list': latest_poll_list,
-##		})
-##	return HttpResponse(t.render(c))
-##	#output = ', '.join([p.question for p in latest_poll_list])
-##	#return HttpResponse(output)
-##	#return HttpResponse("Hello, world.  You're at the very cool poll index.")
-#
-#def detail(request, poll_id):
-#	p = get_object_or_404(Poll, pk=poll_id)
-#	return render_to_response('polls/detail.html', {'poll': p},
-#			context_instance=RequestContext(request))
-#	#try:
-#	#	p = Poll.objects.get(pk=poll_id)
-#	#except Poll.DoesNotExist:
-#	#	raise Http404
-#	#return render_to_response('polls/detail.html', {'poll': p})
-#	#return HttpResponse("You're looking at the (very cool) poll %s." % poll_id)
-#
-#def results(request, poll_id):
-#	p = get_object_or_404(Poll, pk=poll_id)
-#	return render_to_response('polls/results.html', {'poll': p})
-##return HttpResponse("You're looking at the (even cooler) results of the (very cool) poll %s." % poll_id)
+class SoftwareDetailView(DetailView):
+    template_name = 'software/detail.html'
+    queryset = Software.objects.filter(is_published=True).order_by('title')
 
-#def vote(request, poll_id):
-#	p = get_object_or_404(Poll, pk=poll_id)
-#	try:
-#		selected_choice = p.choice_set.get(pk=request.POST['choice'])
-#	except (KeyError, Choice.DoesNotExist):
-#		# Redisplay the poll voting form.
-#		return render_to_response('polls/detail.html', {
-#			'poll': p,
-#			'error_message': "You didn't select a (very cool) choice.",
-#			}, context_instance=RequestContext(request))
-#	else:
-#		selected_choice.votes += 1
-#		selected_choice.save()
-#		return HttpResponseRedirect(reverse('poll_results', args=(p.id,)))
-#		#return HttpResponseRedirect(reverse('polls.views.results', args=(p.id,)))
-#	
-#	return HttpResponse("You're voting on (the very-soon-to-be-very-cool) poll %s." % poll_id)
-#
+    def dispatch(self, request, *args, **kwargs):
+        # request.breadcrumbs(('Software', reverse('software')), (self.get_queryset().get_title(), reverse('software_detail')))
+        return super(SoftwareDetailView, self).dispatch(request, *args, **kwargs)
 
-# Create your views here.
+class SoftwareTagView(ListView):
+    template_name = 'software/software_list.html'
+    context_object_name = 'software_list'
+    allow_empty = True
 
-#def detail(request, slug):
-#	s = get_object_or_404(Software, pk=slug)
-#	#request.breadcrumbs(_("About"), request.path_info)
-#	return render_to_response('software/detail.html', {'software': s })
-##	p = get_object_or_404(Poll, pk=poll_id)
-##	return render_to_response('polls/results.html', {'poll': p})
+    def dispatch(self, request, *args, **kwargs):
+        # request.breadcrumbs('Software', reverse('software'))
+        try:
+            self.tag = kwargs.pop('tag')
+        except KeyError:
+            raise http404
+        return super(SoftwareTagView, self).dispatch(request, *args, **kwargs)
+
+    def get_queryset(self):
+        queryset = Software.objects.filter(is_published=True).order_by('title')
+        # software = get_object_or_404(queryset, tags__=self.args[0])
+        return queryset.filter(tags__contains=self.tag)
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(SoftwareTagView, self).get_context_data(**kwargs)
+        # Add in the publisher
+        context['tag'] = self.tag
+        return context
