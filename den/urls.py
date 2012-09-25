7858161b83b84e73634b4f0fff8a854b0591c958
Andrew Merenbach <andrew@merenbach.com>
2012-09-24 21:20:50 -0700

    Improve sitemap

--- den/urls.py
+++ den/urls.py
@@ -31,17 +31,17 @@ from photologue.sitemaps import photologue_gallery, photologue_photo
 
 sitemaps = {
     'pages': FlatPageSitemap,
-    'zinnia': EntrySitemap,
+    'blog': EntrySitemap,
     'software': SoftwareSitemap,
-    'photologue_gallery': photologue_gallery,
-    'photologue_photo': photologue_photo,
+    'galleries': photologue_gallery,
+    'photos': photologue_photo,
 }
 
 # Examples:
 # url(r'^$', 'project.views.home', name='home'),
 # url(r'^project/', include('project.foo.urls')),
 urlpatterns = patterns('',
-	url(dajaxice_config.dajaxice_url, include('dajaxice.urls')),
+    url(dajaxice_config.dajaxice_url, include('dajaxice.urls')),
     (r'^contact/', include('contact.urls')),
     (r'^ciphers/', include('ciphers.urls')),
     (r'^software/', include('software.urls')),
@@ -53,11 +53,12 @@ urlpatterns = patterns('',
         ),
         name='haystack_search'
     ),
-	url(r'^xmlrpc/$', 'django_xmlrpc.views.handle_xmlrpc'),
-    url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
+    url(r'^xmlrpc/$', 'django_xmlrpc.views.handle_xmlrpc'),
+    (r'^sitemap\.xml$', 'django.contrib.sitemaps.views.index', {'sitemaps': sitemaps}),
+    (r'^sitemap-(?P<section>.+)\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-	url(r'^comments/', include('django.contrib.comments.urls')),
+    url(r'^comments/', include('django.contrib.comments.urls')),
     (r'^', include('zinnia.urls')),
     (r'^', include('photologue.urls')),
 )
