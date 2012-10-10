6c0d7967b896541c61b968d6d68094c1f7357794
Andrew Merenbach <andrew@merenbach.com>
2012-10-10 10:37:21 -0700

    Add fabric command to deploy

--- fabfile.py
+++ fabfile.py
@@ -1,45 +1,11 @@
-REPOS = (("project", "origin", "master"),)
-        #("my_app", "origin", "master"))
+from fabric.api import *
+import os
 
-def production():
-    config.fab_hosts = ['merenbach.com']
-    #config.fab_hosts = ['a.example.com']
-    config.repos = REPOS
-
-def staging():
-    config.fab_hosts = ['a.staging_example.com']
-    config.repos = REPOS
-
-def git_pull():
-    "Updates the repository."
-    run("cd ~/git/$(repo)/; git pull $(parent) $(branch)")
-
-def git_reset():
-    "Resets the repository to specified version."
-    run("cd ~/git/$(repo)/; git reset --hard $(hash)")
-
-def reboot():
-    "Reboot Apache2 server."
-    sudo("apache2ctl graceful")
+PROJECT_PATH = os.path.join('/', 'srv', 'www', 'merenbach.com', 'django', 'den')
 
 def pull():
-    require('fab_hosts', provided_by=[production])
-    for repo, parent, branch in config.repos:
-        config.repo = repo
-        config.parent = parent
-        config.branch = branch
-        invoke(git_pull)
+    with cd(PROJECT_PATH):
+        local('git pull origin master')
+        local('python manage.py collectstatic --noinput')
+	local('touch den/wsgi.py')
 
-def test():
-    local("python manage.py test", fail='abort')
-
-def reset(repo, hash):
-    """
-    Reset all git repositories to specified hash.
-    Usage:
-        fab reset:repo=my_repo,hash=etcetc123
-    """
-    require("fab_hosts", provided_by=[production])
-    config.hash = hash
-    config.repo = repo
-    invoke(git_reset)
