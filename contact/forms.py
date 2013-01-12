e6deb600e5bf06f4dbdf85061a08b7c1ec431816
Andrew Merenbach <andrew@merenbach.com>
2013-01-11 21:30:34 -0800

    Modularize, simplify context-handling in Contact
    - Rewrite template with better Bootstrap structure adherence
    - Make success/failure messages render with proper success/error classes
    - Add CC self checkbox
    - Spread functionality out between methods
    - More!

--- contact/forms.py
+++ contact/forms.py
@@ -4,7 +4,6 @@ from django.utils.safestring import mark_safe
 from contact.decorators import decorate_bound_field
 decorate_bound_field()
 
-
 # decorate_bound_field()
 # class LabelErrorList(ErrorList):
 # def __unicode__(self):
@@ -33,7 +32,7 @@ class ContactForm(forms.Form):
     subject = forms.CharField(max_length=100)
     message = forms.CharField(widget=forms.Textarea)
     sender = forms.EmailField()
-    # cc_myself = forms.BooleanField(required=False, label='Send me a copy')
+    cc_myself = forms.BooleanField(required=False, label='CC yourself?')
 
     # required_css_class = ''
     error_css_class = 'error'
@@ -43,3 +42,62 @@ class ContactForm(forms.Form):
        if kwargs is not None:
            kwargs_new.update(kwargs)
        super(ContactForm, self).__init__(*args, **kwargs_new)
+
+    def send_email(self, recipient_list):
+        # send email using the self.cleaned_data dictionary
+        subject = self.cleaned_data['subject']
+        message = self.cleaned_data['message']
+        from_email = self.cleaned_data['sender']
+        cc_myself = self.cleaned_data['cc_myself']
+        # Values of extra headers (i.e., reply-to) don't get sanitized. To help
+        # prevent possible header injection attempts (via newline), pass along
+        # the from_email as, well, the from email, which can raise an exception.
+        from django.core.mail import EmailMessage
+        email = EmailMessage(subject, message, from_email, recipient_list, headers={'Reply-To': from_email})
+        email.send()
+        if cc_myself:
+            # Echo to sender separately to avoid divulging email addresses
+            from django.core.mail import send_mail
+            send_mail(subject, message, None, [from_email])
+
+    # def akismet_check(self, request, message, sender, debug=False):
+    #     """ Check a contact form submission with Akismet.  Return Yes for spam (or error), No for ham. """
+    #     rv = True
+    #     from akismet import Akismet
+    #     api = Akismet(settings.AKISMET_API_KEY, settings.AKISMET_BLOG_URL)
+    #     #agent='Django contact form'
+    #     if api.verify_key():
+    #         try:
+    #             data = {
+    #                 'user_ip': request.META.get('REMOTE_ADDR', '127.0.0.1'),
+    #                 'user_agent': request.META.get('USER_AGENT', ''),
+    #                 'referrer': request.META.get('HTTP_REFERER', ''),
+    #                 'comment_type': 'comment',
+    #                 'comment_author': sender,
+    #             }
+    #             rv = api.comment_check(message.encode('utf-8'), data=data, build_data=True, DEBUG=debug)
+    #         except:
+    #             pass
+    #     return rv
+
+"""
+    from akismet import Akismet
+
+    api = Akismet(agent='Test Script')
+    # if apikey.txt is in place,
+    # the key will automatically be set
+    # or you can call api.setAPIKey()
+    #
+    if api.key is None:
+        print "No 'apikey.txt' file."
+    elif not api.verify_key():
+        print "The API key is invalid."
+    else:
+        # data should be a dictionary of values
+        # They can all be filled in with defaults
+        # from a CGI environment
+        if api.comment_check(comment, data):
+            print 'This comment is spam.'
+        else:
+            print 'This comment is ham.'
+"""
--- contact/views.py
+++ contact/views.py
@@ -5,7 +5,7 @@ from django.core.urlresolvers import reverse, reverse_lazy
 from django.views.generic import TemplateView
 from django.views.generic.edit import FormView
 from contact.forms import ContactForm
-from contact.models import ContactPage, ContactRecipient
+from contact.models import ContactPage
 
 class ContactView(FormView):
     template_name = 'contact/contact.html'
@@ -15,83 +15,23 @@ class ContactView(FormView):
     def dispatch(self, request, *args, **kwargs):
         try:
             """ Verify that an entry for this site exists """
-            page = ContactPage.objects.get(site__id__exact=get_current_site(request).id)
+            self.contact_page = ContactPage.objects.get(site__id__exact=get_current_site(request).id)
+            self.recipients = [str(r) for r in self.contact_page.recipients.all()]
         except ContactPage.DoesNotExist:
             raise Http404
         request.breadcrumbs('Contact', reverse('contact'))
         return super(ContactView, self).dispatch(request, *args, **kwargs)
 
     def form_valid(self, form):
-        """ This method is called when valid form data has been POSTed. """
+        # This method is called when valid form data has been POSTed.
         # It should return an HttpResponse.
-        if form.is_valid(): # All validation rules pass
-            subject = form.cleaned_data['subject']
-            message = form.cleaned_data['message']
-            sender = form.cleaned_data['sender']
-            # cc_myself = form.cleaned_data['cc_myself']
-
-            # This will throw an exception if the site doesn't exist
-            page = ContactPage.objects.get(site__id__exact=get_current_site(self.request).id)
-
-            if page:
-                recipients = [str(r) for r in page.recipients.all()]
-                # from django.core.mail import send_mail
-                #send_mail(subject, message, sender, recipients)
-                # headers = {
-                #     'Reply-To': sender,
-                #     # 'X-Spam-Flag': 'Yes' if self.akismet_check(self.request, message, sender) else 'No',
-                # }
-                from django.core.mail import EmailMessage
-                # email = EmailMessage(subject, message, sender, recipients, headers=headers)
-                email = EmailMessage(subject, message, sender, recipients, headers={'Reply-To': sender})
-                email.send()
-                # return HttpResponseRedirect(reverse('contact-thanks')) # Redirect after POST
-                # if cc_myself:
-                #     # Echo to sender separately to avoid divulging email addresses
-                #     from django.core.mail import send_mail
-                #     send_mail(subject, message, sender, [sender])
+        form.send_email(self.recipients)
         return super(ContactView, self).form_valid(form)
 
     def get_context_data(self, **kwargs):
         context = super(ContactView, self).get_context_data(**kwargs)
-        current_site_id = get_current_site(self.request).id
-
-        try:
-            context['message'] = ContactPage.objects.get(site__id__exact=current_site_id).message_text
-        except ContactPage.DoesNotExist:
-            pass
-
-        # try:
-        #     context['success'] = ContactPage.objects.get(site__id__exact=current_site_id).success_text
-        # except ContactPage.DoesNotExist:
-        #     pass
-
-        try:
-            context['failure'] = ContactPage.objects.get(site__id__exact=current_site_id).failure_text
-        except ContactPage.DoesNotExist:
-            pass
-
+        context['contact_page'] = self.contact_page
         return context
-
-    def akismet_check(self, request, message, sender, debug=False):
-        """ Check a contact form submission with Akismet.  Return Yes for spam (or error), No for ham. """
-        rv = True
-        from akismet import Akismet
-        api = Akismet(settings.AKISMET_API_KEY, settings.AKISMET_BLOG_URL)
-        #agent='Django contact form'
-        if api.verify_key():
-            try:
-                data = {
-                    'user_ip': request.META.get('REMOTE_ADDR', '127.0.0.1'),
-                    'user_agent': request.META.get('USER_AGENT', ''),
-                    'referrer': request.META.get('HTTP_REFERER', ''),
-                    'comment_type': 'comment',
-                    'comment_author': sender,
-                }
-                rv = api.comment_check(message.encode('utf-8'), data=data, build_data=True, DEBUG=debug)
-            except:
-                pass
-        return rv
 
 class ContactThanksView(TemplateView):
     template_name = 'contact/thanks.html'
@@ -99,7 +39,7 @@ class ContactThanksView(TemplateView):
     def dispatch(self, request, *args, **kwargs):
         try:
             """ Verify that an entry for this site exists """
-            page = ContactPage.objects.get(site__id__exact=get_current_site(request).id)
+            self.contact_page = ContactPage.objects.get(site__id__exact=get_current_site(request).id)
         except ContactPage.DoesNotExist:
             raise Http404
         request.breadcrumbs([('Contact', reverse('contact')), ('Thanks', reverse('contact-thanks'))])
@@ -107,43 +47,5 @@ class ContactThanksView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(ContactThanksView, self).get_context_data(**kwargs)
-        current_site_id = get_current_site(self.request).id
-
-        try:
-            context['message'] = ContactPage.objects.get(site__id__exact=current_site_id).message_text
-        except ContactPage.DoesNotExist:
-            pass
-
-        try:
-            context['success'] = ContactPage.objects.get(site__id__exact=current_site_id).success_text
-        except ContactPage.DoesNotExist:
-            pass
-
-        # try:
-        #     context['failure'] = ContactPage.objects.get(site__id__exact=current_site_id).failure_text
-        # except ContactPage.DoesNotExist:
-        #     pass
-
+        context['contact_page'] = self.contact_page
         return context
-
-"""
-    from akismet import Akismet
-    
-    api = Akismet(agent='Test Script')
-    # if apikey.txt is in place,
-    # the key will automatically be set
-    # or you can call api.setAPIKey()
-    #
-    if api.key is None:
-        print "No 'apikey.txt' file."
-    elif not api.verify_key():
-        print "The API key is invalid."
-    else:
-        # data should be a dictionary of values
-        # They can all be filled in with defaults
-        # from a CGI environment
-        if api.comment_check(comment, data):
-            print 'This comment is spam.'
-        else:
-            print 'This comment is ham.'
-"""
