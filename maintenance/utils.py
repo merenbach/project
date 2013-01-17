6bf40bd632db644b15f167c975b736d288067fe6
Andrew Merenbach <andrew@merenbach.com>
2013-01-16 20:44:14 -0800

    Add proper attributions

--- maintenance/utils.py
+++ maintenance/utils.py
@@ -1,11 +1,15 @@
 # Utilities for maintenance
 
 def get_current_site_messages(request):
+    """ Cover for get_site_messages """
     from django.contrib.sites.models import get_current_site
     return get_site_messages(get_current_site(request))
 
 def get_site_messages(site):
-    """ Retrieve messages for the current site, updating the cache as necessary """
+    """
+    Retrieve messages for the current site, updating the cache as necessary.
+    Adapted from inner logic of `thinkjson/django-maintenance` (on Github).
+    """
     from django.conf import settings
     from django.core.cache import cache
     from django.utils import timezone
@@ -14,7 +18,7 @@ def get_site_messages(site):
     if getattr(settings, 'MAINTENANCE_CACHE_MESSAGES', False):
         messages = cache.get('maintenance_messages')
     else:
-        """ This will otherwise be undefined """
+        # This will otherwise be undefined
         messages = None
     if not messages:
         messages = MaintenanceMessage.objects.filter(sites__id=site.id)\
@@ -28,6 +32,7 @@ def is_privileged_request(request):
     """
     Superusers get unconditional access. Staff by default do not. This is
     configurable in `settings.py`, but the setting is completely optional.
+    This, too, is adapted from `thinkjson/django-maintenance` (on Github).
     """
     from django.conf import settings
     disable_for_staff = getattr(settings, 'MAINTENANCE_DISABLE_FOR_STAFF', False)
