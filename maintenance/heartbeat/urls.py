8eff4f4cd4057328d55cb205923e69c2bb4a00cf
Andrew Merenbach <andrew@merenbach.com>
2013-01-18 23:43:55 -0800

    Update deprecated urls API

--- maintenance/heartbeat/urls.py
+++ maintenance/heartbeat/urls.py
@@ -1,6 +1,6 @@
 # urls for optional maintenance add-on to monitor site uptime
 
-from django.conf.urls.defaults import patterns, url, include
+from django.conf.urls import patterns, url, include
 from django.views.decorators.cache import never_cache
 from maintenance.heartbeat.views import HeartbeatView
 
