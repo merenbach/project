03a10b8fce12d74a3a6555a15a350fb1b46cbfee
Andrew Merenbach <andrew@merenbach.com>
2013-01-24 22:20:07 -0800

    Add timezone context processor

--- common/settings_base.py
+++ common/settings_base.py
@@ -71,12 +71,12 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.debug',
     'django.core.context_processors.i18n',
-    'django.core.context_processors.request',
     'django.core.context_processors.media',
     'django.core.context_processors.static',
-    'sekizai.context_processors.sekizai',
-    'django.core.context_processors.csrf',
+    'django.core.context_processors.tz',
     'django.contrib.messages.context_processors.messages',
+    'django.core.context_processors.request',
+    'sekizai.context_processors.sekizai',
     'common.context_processors.site',
 )
 
