7a6265c8523f6a3351cd4fdd0bd48508f5feb50f
Andrew Merenbach <andrew@merenbach.com>
2013-01-28 22:00:43 -0800

    Add Zinnia to liz's apps

--- liz/settings_base.py
+++ liz/settings_base.py
@@ -37,9 +37,47 @@ ROOT_URLCONF = 'liz.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'liz.wsgi.application'
 
+TEMPLATE_CONTEXT_PROCESSORS += (
+    'zinnia.context_processors.version',    # optional
+)
+
 INSTALLED_APPS += (
     'liz', # custom additions and utilities
+    'tagging',
+    'mptt',
+    'zinnia',
 )
+
+# Zinnia
+ZINNIA_MARKUP_LANGUAGE = 'markdown'
+ZINNIA_WYSIWYG = False
+
+from zinnia.xmlrpc import ZINNIA_XMLRPC_METHODS
+XMLRPC_METHODS = ZINNIA_XMLRPC_METHODS + [
+    ('zinnia.xmlrpc.metaweblog.new_category',
+     'mt.addCategory'),
+    ('zinnia.xmlrpc.metaweblog.get_categories',
+     'mt.getCategoryList'),
+    ('den.xmlrpc.mt.get_post_categories',
+     'mt.getPostCategories'),
+    ('den.xmlrpc.mt.set_post_categories',
+     'mt.setPostCategories'),
+    ('den.xmlrpc.mt.new_post',
+     'metaWeblog.newPost'),
+    ('den.xmlrpc.mt.edit_post',
+     'metaWeblog.editPost'),
+    ('den.xmlrpc.mt.get_post',
+     'metaWeblog.getPost'),
+    ('den.xmlrpc.mt.get_recent_posts',
+     'metaWeblog.getRecentPosts'),
+]
+
+# Pagination
+# http://packages.python.org/linaro-django-pagination/usage.html#how-to-use-linaro-django-pagination
+PAGINATION_INVALID_PAGE_RAISES_404 = True
+# PAGINATION_PREVIOUS_LINK_DECORATOR = '&laquo;'
+# PAGINATION_NEXT_LINK_DECORATOR = '&raquo;'
+PAGINATION_CLEAN_URL = False
 
 # Haystack
 #HAYSTACK_CONNECTIONS = {
