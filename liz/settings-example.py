bc1c54be151042cf3df638250995864d976e1982
Andrew Merenbach <andrew@merenbach.com>
2013-01-28 21:39:36 -0800

    Add new site for lizcheney

---
+++ liz/settings-example.py
@@ -0,0 +1,90 @@
+# Django settings for project project.
+
+# import central settings
+from settings_base import *
+
+# for url/path manipulation
+from urlparse import urljoin
+from os.path import join
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    # ('Your Name', 'your_email@example.com'),
+)
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = '/srv/www/example.com/media/'
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+if not DEBUG:
+    MEDIA_URL = 'http://cdn.example.com/'
+else:
+    MEDIA_URL = 'http://media.example.com/'
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = join(MEDIA_ROOT, 'static')
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = urljoin(MEDIA_URL, 'static/')
+
+# deprecated but apparently still necessary for some things
+ADMIN_MEDIA_PREFIX = urljoin(STATIC_URL, 'admin/')
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = ''
+
+# Disqus
+DISQUS_API_KEY = ''
+DISQUS_WEBSITE_SHORTNAME = 'merenbach'
+
+# Akismet
+AKISMET_API_KEY = ''
+AKISMET_BLOG_URL = 'http://www.merenbach.com'
+
+# Clicky
+CLICKY_SITE_ID = ''
+# CLICKY_RENDER_NON_JS_CODE = False
+
+# Google Analytics
+GOOGLE_ANALYTICS_PROPERTY_ID = ''
+# GOOGLE_ANALYTICS_SITE_SPEED = False
+
+# my customizations
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'noreply@merenbach.com'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'noreply@merenbach.com'
+SERVER_EMAIL = 'noreply@merenbach.com'
+
+# caching
+#CACHES = {
+#        'default': {
+#            'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+#            'LOCATION': '127.0.0.1:11211',
+#            #'LOCATION': 'unix:/tmp/memcached.sock',
+#            }
+#        }
+
---
+++ liz/settings_base.py
@@ -0,0 +1,113 @@
+# Django settings for project project.
+
+# Try to import common settings
+try:
+    from common.settings_base import *
+except:
+    raise SystemExit("Error reading common settings!")
+
+import os
+import sys
+
+# get the settings path
+PROJECT_ROOT = os.path.realpath(os.path.dirname(__file__))
+PROJECT_PATH = os.path.abspath(PROJECT_ROOT)
+
+if PROJECT_PATH not in sys.path:
+    sys.path.insert(0, PROJECT_PATH)
+
+# add to default paths
+#paths = (
+#   PROJECT_PATH,
+#        #os.path.realpath(os.path.dirname(PROJECT_PATH)),
+#        )
+#
+#for path in paths:
+#    if path not in sys_path:
+#        sys_path.insert(0, path)
+
+SITE_ID = 4
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_PATH, 'static'),
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.cache.UpdateCacheMiddleware',
+    'django.middleware.gzip.GZipMiddleware',
+    'django.middleware.http.ConditionalGetMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    #'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
+    # Uncomment the next line for simple clickjacking protection:
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    #'pagination.middleware.PaginationMiddleware',
+    'linaro_django_pagination.middleware.PaginationMiddleware',
+    'breadcrumbs.middleware.BreadcrumbsMiddleware',
+    #'breadcrumbs.middleware.FlatpageFallbackMiddleware',
+    'maintenance.middleware.MaintenanceMiddleware',
+    'django.middleware.transaction.TransactionMiddleware',
+    'django.middleware.cache.FetchFromCacheMiddleware',
+)
+
+# Set a caching key prefix to avoid collisions
+CACHE_MIDDLEWARE_KEY_PREFIX = 'liz'
+
+ROOT_URLCONF = 'liz.urls'
+
+# Python dotted path to the WSGI application used by Django's runserver.
+WSGI_APPLICATION = 'liz.wsgi.application'
+
+TEMPLATE_DIRS = (
+    os.path.join(PROJECT_PATH, 'templates'),
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
+INSTALLED_APPS += (
+    'liz', # custom additions and utilities
+)
+
+# Haystack
+#HAYSTACK_CONNECTIONS = {
+#        'default': {
+#            'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
+#            'PATH': os.path.join(PROJECT_ROOT, 'whoosh_index'),
+#            # 'PATH': os.path.join(os.path.dirname(__file__), 'whoosh_index'),
+#            'TIMEOUT': 60 * 5,
+#            'INCLUDE_SPELLING': True,
+#            'BATCH_SIZE': 100,
+#            },
+#        }
+
+#HAYSTACK_SEARCH_RESULTS_PER_PAGE = 10
+##HAYSTACK_SEARCH_RESULTS_PER_PAGE = 5
+##HAYSTACK_ROUTERS = ['core.routers.SiteRouter']
+
+
+#MEDIA_CDN_DOMAIN = 'http://cdn.merenbach.com/'
+#DJANGO_STATIC_FILE_PROXY = 'den.cdn.cdn_origin_pull_file_proxy'
+
+#import logging
+#logging.basicConfig(level=logging.DEBUG)
+
+# Pagination
+# http://packages.python.org/linaro-django-pagination/usage.html#how-to-use-linaro-django-pagination
+#PAGINATION_INVALID_PAGE_RAISES_404 = True
+## PAGINATION_PREVIOUS_LINK_DECORATOR = '&laquo;'
+## PAGINATION_NEXT_LINK_DECORATOR = '&raquo;'
+#PAGINATION_CLEAN_URL = False
+
+# import warnings
+# warnings.filterwarnings(
+#         'error', r"DateTimeField received a naive datetime",
+#         RuntimeWarning, r'django\.db\.models\.fields')
+
