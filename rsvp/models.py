4da3968a69a33d9a784c014b2f30620c093ffdd2
Andrew Merenbach <andrew@merenbach.com>
2013-02-03 19:56:52 -0800

    Add token recognition

--- rsvp/models.py
+++ rsvp/models.py
@@ -1,17 +1,14 @@
 from django.db import models
-
 from django.conf import settings
-
-from tagging.fields import TagField
-from tagging.models import Tag
-
 from django.contrib.sites.models import Site
+from rsvp.utils import create_token
 
 class Person(models.Model):
     """ Represent an invitee """
-    name = models.CharField(max_length=100, blank=F0alse)
+    name = models.CharField(max_length=100, blank=False)
     email = models.EmailField(max_length=254, blank=True, unique=True)
     is_attending = models.BooleanField()
+    is_on_inner_envelope = models.BooleanField(default=True)
 
     def __unicode__(self):
         return u'{0} <{1}>'.format(self.name, self.email)
@@ -20,18 +17,23 @@ class Party(models.Model):
     """ Represent a party """
     name = models.CharField(max_length=100, blank=False)
     members = models.ManyToManyField(Person, blank=True, null=True, help_text='Members of a party.')
-    
+    token = models.CharField(max_length=64, blank=False, editable=False, default=create_token)
+    creation_date = models.DateField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'parties'
+
     @property
     def headcount(self):
     	""" Return a headcount for the party """
     	return members.filter_by(is_attending=True).count()
 
     def __unicode__(self):
-        return u'Party {0}'.format(self.name)
+        return u'{0}'.format(self.name)
 
 class RSVP(models.Model):
     """ Represent an invitee """
-    site = models.ForeignKey(Site)
+    site = models.ForeignKey(Site, default=Site.objects.get_current)
     party = models.ForeignKey(Party, unique=True)
     message = models.TextField(blank=True, help_text='An optional message for the happy couple.')
     rsvp_date = models.DateField(auto_now=True)
--- rsvp/views.py
+++ rsvp/views.py
@@ -1,1 +1,18 @@
-# Create your views here.
+from django.views.generic import TemplateView
+from django.http import Http404
+from rsvp.models import Party
+
+class RespondezView(TemplateView):
+	template_name = 'rsvp/respondez.html'
+
+	def dispatch(self, request, *args, **kwargs):
+		try:
+			self.party = Party.objects.get(token__exact=kwargs.get('token', None))
+		except Party.DoesNotExist:
+			raise Http404
+		return super(RespondezView, self).dispatch(request, *args, **kwargs)
+
+	def get_context_data(self, **kwargs):
+		context = super(RespondezView, self).get_context_data(**kwargs)
+		context['party'] = self.party
+		return context
