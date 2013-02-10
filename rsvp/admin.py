350c4dc49935dbb2ef59c35b1ea974eb65204a9c
Andrew Merenbach <andrew@merenbach.com>
2013-02-09 23:12:55 -0800

    Add more admin options for marking

--- rsvp/admin.py
+++ rsvp/admin.py
@@ -21,6 +21,16 @@ def mark_uninvited(modeladmin, request, queryset):
     queryset.update(is_invited=False)
 mark_uninvited.short_description = "Mark selected parties as uninvited"
 
+def mark_confirmed(modeladmin, request, queryset):
+    """ Change attributes of selected parties """
+    queryset.update(is_confirmed=True)
+mark_confirmed.short_description = "Mark selected parties as confirmed"
+
+def mark_unconfirmed(modeladmin, request, queryset):
+    """ Change attributes of selected parties """
+    queryset.update(is_confirmed=False)
+mark_unconfirmed.short_description = "Mark selected parties as unconfirmed"
+
 def mark_attending(modeladmin, request, queryset):
     """ Change attributes of selected party members """
     queryset.update(is_attending=True)
@@ -31,17 +41,27 @@ def mark_nonattending(modeladmin, request, queryset):
     queryset.update(is_attending=False)
 mark_nonattending.short_description = "Mark selected party members as nonattending"
 
+def mark_party_leader(modeladmin, request, queryset):
+    """ Change attributes of selected party members """
+    queryset.update(is_party_leader=True)
+mark_party_leader.short_description = "Mark selected party members as party leaders"
+
+def mark_not_party_leader(modeladmin, request, queryset):
+    """ Change attributes of selected party members """
+    queryset.update(is_party_leader=False)
+mark_not_party_leader.short_description = "Mark selected party members as not party leaders"
+
 class PartyAdmin(admin.ModelAdmin):
     list_display = ('name', 'alignment', 'is_invited', 'size', 'is_confirmed', 'headcount', 'last_modified')
-    actions = (mark_invited, mark_uninvited)
+    actions = (mark_invited, mark_uninvited, mark_confirmed, mark_unconfirmed)
 
 class InviteeAdmin(admin.ModelAdmin):
     fieldsets = (
         (None, {'fields': ('name', 'email', 'is_party_leader')}),
         ('Response card', {'fields' : ('is_attending',)}),
     )
-    list_display = ('name', 'email', 'is_attending', 'is_party_leader')
-    actions = (mark_attending, mark_nonattending)
+    list_display = ('name', 'email', 'is_party_leader', 'is_attending')
+    actions = (mark_attending, mark_nonattending, mark_party_leader, mark_not_party_leader)
 
 class InvitationAdmin(admin.ModelAdmin):
     fieldsets = (
