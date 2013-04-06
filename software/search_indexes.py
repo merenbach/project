f3d5708f1b97038c49f1c42ee84c41a2598b3a70
Andrew Merenbach <andrew@merenbach.com>
2013-04-06 14:00:47 -0700

    Clean up Software search indices

--- software/search_indexes.py
+++ software/search_indexes.py
@@ -1,22 +1,14 @@
-from django.utils import timezone
-from django.conf import settings
 from haystack import indexes
+from django.utils import timezone
 from software.models import Software
 
 class SoftwareIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     title = indexes.CharField(model_attr='title')
-    #title = indexes.CharField(model_attr='title')
     url = indexes.CharField(model_attr='get_absolute_url')
-    #title = CharField(model_attr='title')
-    #description = CharField(model_attr='description')
-    #author = CharField(model_attr='user')
     pub_date = indexes.DateTimeField(model_attr='pub_date')
     pub_site = indexes.IntegerField(model_attr='site__id')
     is_published = indexes.BooleanField(model_attr='is_published')
-
-    def prepare_pub_site(self, obj):
-        return obj.site.id
 
     def get_model(self):
         return Software
