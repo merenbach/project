aacd8d0d42523d10024fb4080e30717f9f614839
Andrew Merenbach <andrew@merenbach.com>
2013-04-06 16:33:37 -0700

    Adjust static paths to add slash

--- tata/settings_base.py
+++ tata/settings_base.py
@@ -52,7 +52,7 @@ STATIC_URL = urljoin(MEDIA_URL, 'static/')
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = join(MEDIA_ROOT, 'static')
+STATIC_ROOT = join(MEDIA_ROOT, 'static/')
 
 # Additional locations of static files
 STATICFILES_DIRS = (
