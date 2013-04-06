4b703ffb89be3fd5cbcf0174695ebca01965b132
Andrew Merenbach <andrew@merenbach.com>
2013-04-06 14:36:46 -0700

    Filter other sites' flatpages from search indices

--- den/search_indexes.py
+++ den/search_indexes.py
@@ -1,22 +1,20 @@
 from haystack import indexes
 from django.contrib.flatpages.models import FlatPage
+from django.contrib.sites.models import Site
+from photologue.models import Gallery
 from zinnia.models import Entry
 
 class FlatPageIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     title = indexes.CharField(model_attr='title')
     url = indexes.CharField(model_attr='url')
-    sites = indexes.MultiValueField(model_attr='sites__all')
-
-    def prepare_sites(self, obj):
-        return [a.id for a in obj.sites.all()]
 
     def get_model(self):
         return FlatPage
 
     def index_queryset(self, using=None):
         """Used when the entire index for model is updated."""
-        return self.get_model().objects.all()
+        return self.get_model().objects.filter(sites__id=Site.objects.get_current().id)
 
 class EntryIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
@@ -25,15 +23,12 @@ class EntryIndex(indexes.SearchIndex, indexes.Indexable):
     url = indexes.CharField(model_attr='get_absolute_url')
     pub_date = indexes.DateTimeField(model_attr='creation_date')
 
-    #def prepare_sites(self, obj):
-    #    return [a.id for a in obj.sites.all()]
-
     def get_model(self):
         return Entry
 
     def index_queryset(self, using=None):
         """Used when the entire index for model is updated."""
-        return self.get_model().published.all()
+        return self.get_model().published
 
 #class ArticleIndex(indexes.SearchIndex, indexes.Indexable):
 #    text = indexes.CharField(document=True, use_template=True)
@@ -61,12 +56,11 @@ class GalleryIndex(indexes.SearchIndex, indexes.Indexable):
     pub_date = indexes.DateTimeField(model_attr='date_added')
 
     def get_model(self):
-        from photologue.models import Gallery
         return Gallery
 
     def index_queryset(self, using=None):
         """Used when the entire index for model is updated."""
-        return self.get_model().objects.filter(is_public=True).all()
+        return self.get_model().objects.filter(is_public=True)
 
 #class PhotoIndex(indexes.SearchIndex, indexes.Indexable):
 #    text = indexes.CharField(document=True, use_template=True)
